b2a7c8e7566d6a030192ed351a160d6b5f822004
Dev@ Oyunge <Dev@Oyunge@PELAPPS11.protoenergy.local>
2021-08-02 14:19:06 +0300

    sidebar designs

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgU4RCAaVn1UnQGfMAOhTUyS3TE_-gavQ",
+    authDomain: "wattsupclone-f9552.firebaseapp.com",
+    projectId: "wattsupclone-f9552",
+    storageBucket: "wattsupclone-f9552.appspot.com",
+    messagingSenderId: "540604883859",
+    appId: "1:540604883859:web:d340945207248e14993e66"
+  };
